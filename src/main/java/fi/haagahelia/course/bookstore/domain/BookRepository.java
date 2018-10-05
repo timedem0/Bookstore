5b9011a1b601f625645a9b0e1104f71448060df6
Tudor <timedemo@gmail.com>
2018-10-05 21:53:40 +0300

    Added REST exercises

--- src/main/java/fi/haagahelia/course/bookstore/domain/BookRepository.java
+++ src/main/java/fi/haagahelia/course/bookstore/domain/BookRepository.java
@@ -1,7 +1,12 @@
 package fi.haagahelia.course.bookstore.domain;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
 	
+	List<Book> findByAuthor(@Param("author") String author);
 }
--- src/main/java/fi/haagahelia/course/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/course/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package fi.haagahelia.course.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.course.bookstore.domain.Book;
 import fi.haagahelia.course.bookstore.domain.BookRepository;
@@ -50,5 +54,17 @@ public class BookController {
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	book_repository.deleteById(bookId);
         return "redirect:../index";
-    }  
+    }
+    
+	// RESTful services
+    
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) book_repository.findAll();
+    }    
+
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return book_repository.findById(bookId);
+    }   
 }
